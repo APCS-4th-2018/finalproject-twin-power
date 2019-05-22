f1c1caa33820207a26886ff978ed4ed06f9a847d
Julia Du <julia285d@gmail.com>
2019-05-22 12:18:05 -0700

    added method to print the current time and day

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import chn.util.*;
+import java.util.*;
 /**
  * Representation of a single game
  *
@@ -9,10 +11,12 @@ public class Game
     private int day, distance, time;
     private final int WIN_DISTANCE = 1000;
     private Player player;
+    private ArrayList inventory;
 
     public Game(String name)
     {
         player = new Player(name);
+        inventory = new ArrayList();
         day = time = distance = 0;
         System.out.println("Welcome " + player.getName());
         while(player.isAlive() && distance < WIN_DISTANCE)
@@ -23,6 +27,7 @@ public class Game
     
     private void turn()
     {
+        printStatus();
         //print out the choices possible
         //call other methods depending on the choice
     }
@@ -45,6 +50,11 @@ public class Game
             time = time % 24;
             day++;
         }
+    }
+    private void printStatus()
+    {
+        System.out.println("Day " + day);
+        System.out.print("Time: " + time + ":00");
     }
     private void endGame()
     {
