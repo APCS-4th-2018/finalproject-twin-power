5d0e7b2535e9fd0a3c41f40302ed51dc87287837
Ian Xu <ijkxuluo7000@gmail.com>
2019-05-22 12:54:35 -0700

    Added classes

---
+++ Animal.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Animal here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public abstract class Animal
+{
+    private String aName;
+    private String solution;
+    public Animal ()
+    {
+    }
+    
+    public abstract boolean compareTo();
+
+}
