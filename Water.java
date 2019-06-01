0204e5774f7d389c6c8c776d6945bc8ba75dfe94
Julia Du <42658168+julia285d@users.noreply.github.com>
2019-06-01 16:14:53 -0700

    added Javadoc for Item/Water/Food

--- Water.java
+++ Water.java
@@ -1,20 +1,40 @@
 /**
- * Write a description of class Food here.
+ * A Water class that implements the Item interface
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author  Julia Du
+ * @version 06/01/19
  */
 public class Water implements Item
 {
     private String name;
+    
+    /**
+     * Constructor for the Water class
+     * @param str   the name of the Water
+     */
     public Water(String str)
     {
         name = str;
     }
+    
+    /**
+     * Returns the name of the Water
+     * @return name
+     */
     public String getName(){return name;}
+    
+    /**
+     * Uses the Water on a specified Player
+     * @param player    the Player object the Water will be used on
+     */
     public void useItem(Player player)
     {
         player.changeThirst(1.5);
     }
+    
+    /**
+     * Returns the name of the Water
+     * @return name
+     */
     public String toString(){return name;}
 }
