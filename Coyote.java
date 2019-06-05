5cfc447eef30fc26d96873a3b76c96ac63c6136a
Ian Xu <ijkxuluo7000@gmail.com>
2019-06-05 13:14:48 -0700

    Create Coyote.java

---
+++ Coyote.java
@@ -0,0 +1,22 @@
+/**
+ * Here is the class containing description and solution for a Bear
+ *
+ * @author Ian & Kane
+ * @version Twin Power
+ */
+public class Coyote extends Animal
+{
+    /**
+     * Constructor for objects of class Bear
+     */
+    public Coyote()
+    {
+        super("A Coyote has spotted you, and is ready for attack. What should you do?",
+              "Do not run away or back down. Throw something at it while yelling and waving arms",
+              "Do not run away. This may trigger an attack. " + 
+              "Never turn your back to a coyote. " + 
+              "Maintain constant eye contact. " + 
+              "Make loud noises, yell, wave your arms. " +
+              "Make yourself look bigger with your clothing or other objects.");
+    }
+}
